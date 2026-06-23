a34154f205c95e3dc2eeb64ad59f053d18f4815d
Szpruta <9mk92brd9f@privaterelay.appleid.com>
2026-06-23 22:26:52 +0200

    feat: update db schema

---
+++ src/Warehouse.Api/Migrations/20260621212257_Update.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Warehouse.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SKU",
+                table: "Products",
+                newName: "Sku");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Sku",
+                table: "Products",
+                newName: "SKU");
+        }
+    }
+}
